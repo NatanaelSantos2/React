ba88182d8d26fa59c106f979ce10e92378c7ca90
natanael santos <natanaelsantoss2br@gmail.com>
2025-10-15 22:33:19 -0300

    projeto pizza BACKEND

---
+++ BACKEND/src/routes.ts
@@ -0,0 +1,15 @@
+import { Router, Request, Response } from "express";
+
+import { CreateUserController } from "./controllers/user/CreateUserController"
+
+const router = Router();
+
+// router.get("/teste", (req: Request, res: Response) => {
+//   return res.json({ nome: "Lais" });
+//     // throw new Error("alguma mensagem de error")
+// });
+
+//--- ROTAS USER--- 
+router.post("/users", new CreateUserController().handle)
+
+export { router }; 
